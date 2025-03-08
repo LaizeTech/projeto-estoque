c7b534591d4d34de43cf126e40229934ee9c7c1d
AyrtonCasa <ayrton.casa@sptech.school>
2025-03-07 21:00:11 -0300

    fix: fix nas dependências

--- dev/back-end/BackLaizeTech/BackLaizeTech/src/main/kotlin/laize_tech/back/Usuario.kt
+++ dev/back-end/BackLaizeTech/BackLaizeTech/src/main/kotlin/laize_tech/back/Usuario.kt
@@ -1,3 +1,18 @@
 package laize_tech.back
 
-data class Usuario()
+data class Usuario(
+    var nome: String? = null,
+    var email: String? = null,
+    var telefone: String? = null,
+    var acesso_financeiro: Boolean
+    //true significa que tem acesso ao gerenciamento financeiro
+){
+
+    var senha: String = ""
+        private set
+
+    fun setSenha(novaSenha: String){
+        senha = novaSenha
+    }
+
+}
--- dev/back-end/BackLaizeTech/BackLaizeTech/src/main/kotlin/laize_tech/back/UsuarioController.kt
+++ dev/back-end/BackLaizeTech/BackLaizeTech/src/main/kotlin/laize_tech/back/UsuarioController.kt
@@ -1,4 +1,33 @@
 package laize_tech.back
 
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/usuario")
 class UsuarioController {
+
+    val lista_usuario = mutableListOf<Usuario>(
+        Usuario("Ayrton Casa", "ayrton.casa@sptech.school", "111111", false),
+        Usuario("Gabriel Gomes", "gabriel.gcorrea@sptech.school", "2222222", false)
+    )
+
+    @GetMapping("/listar")
+    fun listarUsuarios(): ResponseEntity<List<Usuario>> {
+
+        if(lista_usuario.isEmpty()){
+            return ResponseEntity.status(204).build()
+        }else{
+            return ResponseEntity.status(200).body(lista_usuario)
+        }
+
+    }
+
+    @PostMapping
+    fun criarUsuario(@RequestBody novoUsuario: Usuario): ResponseEntity<Usuario>{
+        lista_usuario.add(novoUsuario)
+        return ResponseEntity.status(201).body(novoUsuario)
+    }
+
+
 }
