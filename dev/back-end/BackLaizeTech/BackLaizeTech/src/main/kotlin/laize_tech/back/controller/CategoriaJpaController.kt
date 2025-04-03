6bed8f083011f9d1e21e73ce1d2d8c63492d7f3e
JoaoLazaroNeto <joao.lazaro@sptech.school>
2025-04-03 02:18:02 -0300

    Adição da CategoriaRepository, CategoriaJpaController e suas funções

--- dev/back-end/BackLaizeTech/BackLaizeTech/src/main/kotlin/laize_tech/back/controller/CategoriaJpaController.kt
+++ dev/back-end/BackLaizeTech/BackLaizeTech/src/main/kotlin/laize_tech/back/controller/CategoriaJpaController.kt
@@ -1,4 +1,54 @@
 package laize_tech.back.controller
 
-class CategoriaJpaController {
+import jakarta.validation.Valid
+import laize_tech.back.entity.Categoria
+import laize_tech.back.entity.Empresa
+import laize_tech.back.repository.CategoriaRepository
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/categorias")
+class CategoriaJpaController (val repositorio: CategoriaRepository) {
+
+    @GetMapping
+    fun get():ResponseEntity<List<Categoria>> {
+        val categorias = repositorio.findAll()
+
+        return if (categorias.isEmpty()){
+            ResponseEntity.status(204).build()
+        } else {
+            ResponseEntity.status(200).body(categorias)
+        }
+    }
+
+    @PostMapping ("/adicionar")
+    fun post(@RequestBody @Valid novaCategoria: Categoria):
+            ResponseEntity<Categoria> {
+
+        val categoria = repositorio.save(novaCategoria)
+        return ResponseEntity.status(201).body(categoria)
+    }
+
+    @PutMapping("/{id}")
+    fun put (@PathVariable id:Int, @RequestBody categoriaAtualizada: Categoria): ResponseEntity<Categoria>{
+
+        if (!repositorio.existsById(id)){
+            return ResponseEntity.status(404).build()
+        }
+
+        categoriaAtualizada.idCategoria = id
+        val categoria = repositorio.save(categoriaAtualizada)
+        return ResponseEntity.status(200).body(categoria)
+    }
+
+    @DeleteMapping ("/{id}")
+    fun delete(@PathVariable id: Int): ResponseEntity<String> {
+        if (repositorio.existsById(id)) {
+            repositorio.deleteById(id)
+            return ResponseEntity.status(204).build()
+        }
+        val mensagem = "Não foi possível deletar a categoria com id $id"
+        return ResponseEntity.status(404).body(mensagem)
+    }
 }
---
+++ dev/back-end/BackLaizeTech/BackLaizeTech/src/main/kotlin/laize_tech/back/repository/CategoriaRepository.kt
@@ -0,0 +1,17 @@
+package laize_tech.back.repository
+
+import jakarta.transaction.Transactional
+import laize_tech.back.entity.Categoria
+import laize_tech.back.entity.Empresa
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
+
+interface CategoriaRepository : JpaRepository<Categoria, Int> {
+
+    @Transactional
+    @Modifying
+    @Query("select e from Categoria e") //JPQL --> NÃO é SQL.
+    fun findAllEmpresas(): List<Empresa>
+
+}
