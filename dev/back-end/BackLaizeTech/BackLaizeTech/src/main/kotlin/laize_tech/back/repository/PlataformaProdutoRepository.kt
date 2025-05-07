d607e7bd9023f4b262a631424ea63b5cb19f61d0
GabrielGomesCorrea <gabriel.gcorrea@sptech.school>
2025-05-06 23:56:14 -0300

    PlataformaProdutoRepository ajustada.

--- dev/back-end/BackLaizeTech/BackLaizeTech/src/main/kotlin/laize_tech/back/repository/PlataformaProdutoRepository.kt
+++ dev/back-end/BackLaizeTech/BackLaizeTech/src/main/kotlin/laize_tech/back/repository/PlataformaProdutoRepository.kt
@@ -10,21 +10,16 @@ interface PlataformaProdutoRepository : JpaRepository<PlataformaProduto, Int> {
 
     @Transactional
     @Modifying
-    @Query("select e from PlataformaProduto e") //JPQL --> NÃO é SQL.
+    @Query("select e from PlataformaProduto e")
     fun findAllPlataformas(): List<PlataformaProduto>
 
-    // Buscar todos os registros de estoque por nome do produto
-    fun findByProdutoNome(nome: String): List<PlataformaProduto>
+    fun findByProdutoNomeProduto(nome: String): List<PlataformaProduto>
 
-    // Buscar todos os registros de estoque por nome da plataforma
-    fun findByPlataformaNome(nome: String): List<PlataformaProduto>
+    fun findByPlataformaNomePlataforma(nome: String): List<PlataformaProduto>
 
-    // Buscar todos os registros de estoque por nome da empresa
     fun findByEmpresaNomeEmpresa(nomeEmpresa: String): List<PlataformaProduto>
 
-    // Buscar um estoque específico por nome da plataforma e nome do produto
-    fun findByPlataformaNomeAndProdutoNome(plataforma: String, produto: String): PlataformaProduto?
+    fun findByPlataformaNomePlataformaAndProdutoNomeProduto(plataforma: String, produto: String): PlataformaProduto?
 
-    // Buscar todos os estoques por ID da empresa
     fun findByEmpresaIdEmpresa(id: Int): List<PlataformaProduto>
 }
