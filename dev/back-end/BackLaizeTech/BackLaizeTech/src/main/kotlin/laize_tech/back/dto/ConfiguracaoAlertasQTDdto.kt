e71c43a72992467d94bf818b58b138e4e5ee4083
GabrielGomesCorrea <gabriel.gcorrea@sptech.school>
2025-08-19 16:47:15 -0300

    Adicionei a API de alertas.

---
+++ dev/back-end/BackLaizeTech/BackLaizeTech/src/main/kotlin/laize_tech/back/dto/ConfiguracaoAlertasQTDdto.kt
@@ -0,0 +1,8 @@
+package laize_tech.back.dto
+
+data class ConfiguracaoAlertasQTDdto(
+    val id: Int?,
+    val quantidadeAmarelo: Int,
+    val quantidadeVermelha: Int,
+    val quantidadeVioleta: Int
+)
