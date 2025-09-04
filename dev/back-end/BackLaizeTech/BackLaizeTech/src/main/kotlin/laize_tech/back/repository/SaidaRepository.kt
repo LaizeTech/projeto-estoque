717b5a4bc21dead74bf9d0c22d44f66de38b8d9e
Rufino-0 <eli.rufino@sptech.school>
2025-09-04 20:16:40 -0300

    Subindo as alteraçoes de select para rodar a aplicaçao

--- dev/back-end/BackLaizeTech/BackLaizeTech/src/main/kotlin/laize_tech/back/repository/SaidaRepository.kt
+++ dev/back-end/BackLaizeTech/BackLaizeTech/src/main/kotlin/laize_tech/back/repository/SaidaRepository.kt
@@ -9,19 +9,11 @@ interface SaidaRepository : JpaRepository<Saida, Int> {
     @Query(
         value = """
             SELECT
-    SUM(s.preco_venda) 
-FROM
-    Saida s 
-WHERE
-    s.dt_venda >= NOW() - INTERVAL 7 DAY 
-    AND s.id_status_venda = (
-        SELECT
-            sv.id_status_venda 
-        FROM
-            StatusVenda sv 
-        WHERE
-            sv.nome_status = 'FINALIZADA'
-    );
+                SUM(s.preco_venda) AS renda_bruta_7_dias
+            FROM Saida s
+            JOIN StatusVenda sv ON s.id_status_venda = sv.id_status_venda
+            WHERE s.dt_venda >= NOW() - INTERVAL 7 DAY
+            AND sv.nome_status = 'FINALIZADA';
         """,
         nativeQuery = true
     )
