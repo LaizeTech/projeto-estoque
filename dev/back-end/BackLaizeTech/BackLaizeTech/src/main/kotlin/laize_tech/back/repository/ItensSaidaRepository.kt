88c3a0266c770707d9a5b59db865a5740b92a8b0
João Lázaro <160528914+JoaoLazaroNeto@users.noreply.github.com>
2025-10-23 20:07:59 -0300

    Fix: Clean code
    Removed commented-out methods for querying ItensSaida.

--- dev/back-end/BackLaizeTech/BackLaizeTech/src/main/kotlin/laize_tech/back/repository/ItensSaidaRepository.kt
+++ dev/back-end/BackLaizeTech/BackLaizeTech/src/main/kotlin/laize_tech/back/repository/ItensSaidaRepository.kt
@@ -5,24 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository
 
 interface ItensSaidaRepository : JpaRepository<ItensSaida, Int> {
 
-//    @Transactional
-//    @Modifying
-//    @Query("select e from ItensSaida e")
-//    fun findAllItensSaida(): List<ItensSaida>
-//
-//    // Buscar itens por ID da saida
-//    fun findBySaida_IdSaida(idSaida: Int): List<ItensSaida>
-//
-//    // Buscar itens por ID do produto
-//    fun findByProduto_IdProduto(idProduto: Int): List<ItensSaida>
-//
-//    // Buscar todos os itens de uma empresa
-//    fun findByEmpresa_IdEmpresa(idEmpresa: Int): List<ItensSaida>
-//
-//    // Buscar itens por plataforma
-//    fun findByPlataforma_IdPlataforma(idPlataforma: Int): List<ItensSaida>
-//
-//    //Somar o subtotal de todos os itens de uma saida
-//    @Query("SELECT SUM(i.subTotal) FROM ItensSaida i WHERE i.saida.idSaida = :idSaida")
-//    fun somaSubTotalPorSaida(idSaida: Int): Double?
 }
